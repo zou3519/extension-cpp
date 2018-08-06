7e33f54c10bad1c34bbe7cdbf8f911003fe7c2b5
Richard Zou <zou3519@gmail.com>
2018-08-06 06:23:58 -0700

    foo

--- cuda/simplebench2.py
+++ cuda/simplebench2.py
@@ -231,6 +231,8 @@ allocated = torch.cuda.memory_allocated()
 
 sizes = dict(seqLength=100, numLayers=1, hiddenSize=512, miniBatch=64,
              nloops=5, warmup=1, sleep_between=1)
+sizeb = dict(seqLength=5, numLayers=1, hiddenSize=512, miniBatch=64,
+             nloops=5, warmup=1, sleep_between=1)
 benchmark(**sizes)
 
 now_allocated = torch.cuda.memory_allocated()
